699acb0e7b6e8b5da36e276fe7b2f4bf7ccd4fa3
Karam Khan <karamkhan0258@gmail.com>
2024-01-23 16:28:34 +0000

    Skeleton commit

---
+++ WarmUp_01/print_args.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+// Objective: Write the traditional "Hello, World" program.
+void main(void)
+{
+    printf(("Hello, World"));
+}
+
---
+++ WarmUp_02/print_args1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* Objective: Create a program that takes two numbers
+ * as input and performs addition, subtraction,
+ * multiplication, and division.
+*/
+
+void main(){
+    int value;
+    printf("Enter value: ");
+    scanf("%d", &value);
+    printf((const char *) value);
+}
