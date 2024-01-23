f7b55dba598a5d717ca30eb6384d32b3520384b7
Karam Khan <karamkhan0258@gmail.com>
2024-01-23 17:16:41 +0000

    WarmUp_02 Complete

--- WarmUp_02/print_args1.c
+++ WarmUp_02/print_args1.c
@@ -7,8 +7,19 @@
 */
 
 void main(){
-    int value;
-    printf("Enter value: ");
-    scanf("%d", &value);
-    printf((const char *) value);
+    // Initialize the two input variables
+    float num1;
+    float num2;
+    // Print and initialize the two variables
+    // from the user through the console
+    printf("Enter the first value: ");
+    scanf("%f", &num1);
+    printf("Enter the second value: ");
+    scanf("%f", &num2);
+
+    // Print the four operations
+    printf("Sum: %f\n", (num1 + num2));
+    printf("Difference: %f\n", (num1 - num2));
+    printf("Product: %f\n", (num1 * num2));
+    printf("Quotient: %f\n", (num1 / num2));
 }
