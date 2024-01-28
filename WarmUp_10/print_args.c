053283ba3981d396a3727fd19728ad3e7a7b798e
Karam Khan <karamkhan0258@gmail.com>
2024-01-28 20:55:32 +0000

    WarmUp_05 Complete

--- WarmUp_10/print_args.c
+++ WarmUp_10/print_args.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(int argc, char *argv[]) {
+void main(int argc, char *argv[]) {
     for(int i=1; i < argc; i++){
         printf("i=%d argv[%d] = %s\n", i, i, argv[i]);
     }
